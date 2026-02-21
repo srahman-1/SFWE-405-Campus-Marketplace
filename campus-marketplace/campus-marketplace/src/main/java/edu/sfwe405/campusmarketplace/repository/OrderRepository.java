0a0c41c981866069bee691cce34c26f7311d5567
mattalv555 <MateoAlvarez552@gmail.com>
2026-02-21 11:44:07 -0700

    Update OrderRepository.java (fixed some formatting)

--- campus-marketplace/campus-marketplace/src/main/java/edu/sfwe405/campusmarketplace/repository/OrderRepository.java
+++ campus-marketplace/campus-marketplace/src/main/java/edu/sfwe405/campusmarketplace/repository/OrderRepository.java
@@ -2,6 +2,5 @@ package edu.sfwe405.campusmarketplace.repository;
 
 import edu.sfwe405.campusmarketplace.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
+//Order repository for marketplace system (extends jpa repository).
+public interface OrderRepository extends JpaRepository<Order, Long> {}
