00974fd81e9b45aa992d234fec78b2fa0b57628d
wesngu28 <wesley@wesngu28.com>
2026-04-08 09:26:32 -0700

    add dtos

---
+++ campus-marketplace/campus-marketplace/src/main/java/edu/sfwe405/campusmarketplace/dto/LoginResponse.java
@@ -0,0 +1,11 @@
+package edu.sfwe405.campusmarketplace.dto;
+
+import edu.sfwe405.campusmarketplace.model.UserAccount.Role;
+
+public record LoginResponse(
+    String token,
+    String tokenType,
+    long expiresInSeconds,
+    String email,
+    Role role
+) {}
